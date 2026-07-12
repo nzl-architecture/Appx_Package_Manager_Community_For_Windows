4d5e982e8c469854805b9ad4fcc308d82f3593a8
nzl-architecture <3353366044@qq.com>
2026-07-13 05:54:56 +0800

    Add files via upload

---
+++ Release/sparse-pkg/start.bat
@@ -0,0 +1,3 @@
+echo "Welcome to use Appx Package Installer Community!"
+set /p "appxfile=Enter your appx package to install (allowed .appx .msix .appxbundle .msixbundle) :"
+AddAppxPackage.bat %appxfile%
