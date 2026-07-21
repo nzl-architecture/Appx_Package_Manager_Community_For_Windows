552cce0259e8e4fe111cb2bcd6450f0ccd30c28c
nzl-architecture <3353366044@qq.com>
2026-07-22 05:40:23 +0800

    add Fuzzy Matching

--- Release/sparse-pkg/RemoveAppxPackage.bat
+++ Release/sparse-pkg/RemoveAppxPackage.bat
@@ -1,14 +1,18 @@
 @echo off
 net session >nul 2>&1 || (powershell -NoP -C "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c', '\"\"%~f0\" %*\"' -Verb RunAs" && exit)
 cd "%~dp0"
-set /p "packagename=Enter your appx package name (package family name) to uninstall :"
 echo "Select a mode to remove appx package"
 echo "Mode 1: DISM + Remove-ProvisionedAppxPackage (system-level)"
 echo "Mode 2: Powershell + Remove-AppxPackage (per-user)"
 echo "Mode 3: Powershell + Remove-AppxPackage + Custom Params (per-user)"
 echo "Mode 4: Powershell + Remove-AppxPackage + AllUsers (system-level)"
 echo "Mode 5: Powershell + Remove-AppxPackage + AllUsers + Custom Params (system-level)"
+echo "Mode 6: Powershell + Remove-AppxPackage + Fuzzy Matching (per-user)"
+echo "Mode 7: Powershell + Remove-AppxPackage + Fuzzy Matching + Custom Params (per-user)"
+echo "Mode 8: Powershell + Remove-AppxPackage + Fuzzy Matching + AllUsers (system-level)"
+echo "Mode 9: Powershell + Remove-AppxPackage + Fuzzy Matching + AllUsers + Custom Params (system-level)"
 set /p "choice=Select a mode:"
+set /p "packagename=Enter your appx package name (package family name) to uninstall :"
 set params=
 cmd /c echo "Remove appx package %packagename% forever?"
 pause
@@ -31,5 +35,19 @@ powershell.exe -Command "Remove-AppxPackage -Package '%packagename%' -AllUsers"
 if "%choice%"=="5" (
 set /p "params=Enter params for remove appx package %* (system-level):"
 powershell.exe -Command "Remove-AppxPackage -Package '%packagename%' -AllUsers %params%"
+)
+if "%choice%"=="6" (
+powershell.exe -Command "Get-AppxPackage *%packagename%* | Remove-AppxPackage"
+)
+if "%choice%"=="7" (
+set /p "params=Enter params for remove appx package %* (per-user):"
+powershell.exe -Command "Get-AppxPackage *%packagename%* | Remove-AppxPackage %params%"
+)
+if "%choice%"=="8" (
+powershell.exe -Command "Get-AppxPackage *%packagename%* -AllUsers | Remove-AppxPackage -AllUsers"
+)
+if "%choice%"=="9" (
+set /p "params=Enter params for remove appx package %* (system-level):"
+powershell.exe -Command "Get-AppxPackage *%packagename%* -AllUsers | Remove-AppxPackage -AllUsers %params%"
 )
 pause
